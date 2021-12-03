93c780e3b1c7d027bc4d120a9151850f4fd3c88f
Artem <artem.tolpa@gmail.com>
2021-12-03 13:49:00 +0200

    update project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const connection = require("./connection.js");
+const locations = require("./locations.js");
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use("/locations", locations);
+
+const shutdown = () => {
+  console.log("Closing HTTP server");
+  server.close(() => {
+    console.log("Server closed");
+    connection.close(() => {
+      process.exit(0);
+    });
+  });
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+const server = app.listen(port, () => {
+  connection.connect();
+  console.log(`Listening on port ${server.address().port}`);
+});
