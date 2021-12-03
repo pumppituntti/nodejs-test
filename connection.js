8a15c35e34910d957ee8c2112f3c5215c317cb65
Artem <artem.tolpa@gmail.com>
2021-12-03 14:03:39 +0200

    update project

--- connection.js
+++ connection.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DB,
-});
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
+
+const connection = mysql.createPool(config);
 
 let connectionFunctions = {
   connect: () =>
